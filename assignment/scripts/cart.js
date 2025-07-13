fd01be309ba511a5df7b1933787be5609a4a7474
Ahmed Abdille <abdilleahmed779@gmail.com>
2025-07-12 18:13:49 -0700

    homework 4 week 4 part 3 done

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,15 +2,36 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+//function that add items in array and returns true
+
+function addItem(item){
+   basket.push(item);
+   return true;
+}
+addItem("mangoes");
+addItem("potatoes");
+addItem("oranges");
+console.log(`Basket is ${basket}`);
 
 
+// function that loops through the array and returns single item in an array
+  function listItems(basket){
+    for( let i = 0; i < basket.length; i++){
+        console.log(basket[i]) ;
+    }
+}
 
+//function that checks if the value passed is an array
 
+    function empty() {
+      (Array.isArray(basket)) 
+       basket.length = 0;
+    return basket;
+  
+}
 
-
-
-
-
+console.log(empty)
 
 // DO NOT MODIFY
 // Used for automated testing
